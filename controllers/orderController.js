6fad46c180bf4883a700e10d63cf893aadcd39b0
Hoang Trung <lhtrung307@gmail.com>
2019-05-06 07:35:07 +0700

    Create order controller with list, create, detail, delete and update function

---
+++ controllers/orderController.js
@@ -0,0 +1,75 @@
+let Order = require("../models/order");
+
+function handleError(res, err) {
+  res.json({
+    status: "error",
+    message: err
+  });
+}
+
+module.exports.list = (req, res) => {
+  Order.find({}, (err, orders) => {
+    if (err) {
+      return handleError(res, err);
+    }
+    res.status(200).json({
+      message: "Orders retrieve succesfully",
+      data: orders
+    });
+  });
+};
+
+module.exports.create = (req, res) => {
+  var order = new Order(req.body);
+
+  order.save((err) => {
+    if (err) {
+      return handleError(res, err);
+    }
+    res.status(201).json({
+      message: "Order created",
+      data: order
+    });
+  });
+};
+
+module.exports.detail = (req, res) => {
+  Order.findById(req.params.order_id, (err, order) => {
+    if (err) {
+      return handleError(res, err);
+    }
+
+    res.status(200).json({
+      message: "Order retrieve successfully",
+      data: order
+    });
+  });
+};
+
+module.exports.update = (req, res) => {
+  Order.findByIdAndUpdate(
+    req.param.order_id,
+    req.body,
+    { new: true },
+    (err, order) => {
+      if (err) {
+        return handleError(res, err);
+      }
+      res.status(200).json({
+        message: "Order Updated",
+        data: order
+      });
+    }
+  );
+};
+
+module.exports.remove = (req, res) => {
+  Order.findByIdAndDelete(req.params.order_id, (err, order) => {
+    if (err) {
+      return handleError(res, err);
+    }
+    res.status(200).json({
+      message: "Order deleted"
+    });
+  });
+};
