79c0f095702dfc8f92144ccbb70f8674a8314d71
Hoang Trung <lhtrung307@gmail.com>
2019-05-06 08:53:24 +0700

    Create routes.js manage all api endpoints

---
+++ routes.js
@@ -0,0 +1,39 @@
+let router = require("express").Router();
+
+let colorRouter = require("./routes/color");
+let refProductTypeRouter = require("./routes/ref-product-type");
+let productRouter = require("./routes/product");
+let customerRouter = require("./routes/customer");
+let orderRouter = require("./routes/order");
+let refOrderStatusCodeRouter = require("./routes/ref-order-status");
+let orderItemRouter = require("./routes/order-item");
+
+function isEmptyObject(obj) {
+  return Object.keys(obj).length === 0;
+}
+
+let checkReqBody = (req, res, next) => {
+  if (!(req.method === "GET")) {
+    if (isEmptyObject(req.body)) {
+      res.status(405).json({
+        message: "Need body in request"
+      });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+};
+
+router.use(checkReqBody);
+
+router.use(colorRouter);
+router.use(refProductTypeRouter);
+router.use(productRouter);
+router.use(customerRouter);
+router.use(orderRouter);
+router.use(refOrderStatusCodeRouter);
+router.use(orderItemRouter);
+
+module.exports = router;
