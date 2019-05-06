2bed94bcb0dc7d12d083413c29708fa09af01d07
Hoang Trung <lhtrung307@gmail.com>
2019-05-06 14:37:58 +0700

    fix required content to Must have product_type_code

--- models/ref-product-type.js
+++ models/ref-product-type.js
@@ -1,19 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 let refProductTypeSchema = new Schema({
-    product_type_code: {
-        type: String,
-        required: true,
-        trim: true,
-        unique: true
-    },
-    product_type_description: {
-        type: String,
-        required: true
-    }
-
+  product_type_code: {
+    type: String,
+    required: "Must have product_type_code",
+    trim: true,
+    unique: true
+  },
+  product_type_description: {
+    type: String,
+    required: true
+  }
 });
 
-module.exports = mongoose.model('RefProductType', refProductTypeSchema);
+module.exports = mongoose.model("RefProductType", refProductTypeSchema);
