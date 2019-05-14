bceb18a1cc52ff5cf98db7b525653c866d59a0ef
Hoang Trung <lhtrung307@gmail.com>
2019-05-14 15:37:06 +0700

    update the product model: required product type code

--- models/product.js
+++ models/product.js
@@ -1,20 +1,23 @@
-let mongoose = require('mongoose');
+let mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 let productSchema = new Schema({
-    product_type_code: {
-        type: Schema.Types.ObjectId,
-        ref: 'RefProductType'
-    },
-    name: String,
-    price: String,
-    colors: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Color'
-    }],
-    size: String,
-    description: String
+  product_type_code: {
+    type: Schema.Types.ObjectId,
+    ref: "RefProductType",
+    required: "Must have product type code"
+  },
+  name: String,
+  price: String,
+  colors: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Color"
+    }
+  ],
+  size: String,
+  description: String
 });
 
-module.exports = mongoose.model('Product', productSchema)
+module.exports = mongoose.model("Product", productSchema);
