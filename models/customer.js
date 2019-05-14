57c1769741f637f07e49ea63a83d58401c81f5dd
Hoang Trung <lhtrung307@gmail.com>
2019-05-14 15:37:52 +0700

    edit the code

--- models/customer.js
+++ models/customer.js
@@ -1,47 +1,47 @@
-let mongoose = require('mongoose');
+let mongoose = require("mongoose");
 
 let Schema = mongoose.Schema;
 
 let CustomerSchema = new Schema({
-    gender:{
-        type: String,
-        enum: ['Nam', 'Nữ', 'Male', 'Female']
-    },
-    first_name: {
-        type: String,
-        trim: true,
-        required:true
-    },
-    last_name: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    email:{
-        type: String,
-        unique: true,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    phone_number: {
-        type: String,
-        unique: true
-    },
-    address: {
-        type: String,
-        default: ""
-    },
-    county: {
-        type: String,
-        default: ""
-    },
-    country: {
-        type: String,
-        default: ""
-    }
+  gender: {
+    type: String,
+    enum: ["Nam", "Nữ", "Male", "Female"]
+  },
+  first_name: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  last_name: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  phone_number: {
+    type: String,
+    unique: true
+  },
+  address: {
+    type: String,
+    default: ""
+  },
+  county: {
+    type: String,
+    default: ""
+  },
+  country: {
+    type: String,
+    default: ""
+  }
 });
 
-module.exports = mongoose.model('customer', CustomerSchema);
+module.exports = mongoose.model("customer", CustomerSchema);
